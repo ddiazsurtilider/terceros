3d0386595551cf2cd2eb78da1cbeae25cfb61815
ddiazsurtilider <ddiazsurtilider@gmail.com>
2022-09-20 16:59:40 -0500

    se actualizo la vista en las listas-terceros estilos css y html

--- vistas/contenidos/listado-terceros-vista.php
+++ vistas/contenidos/listado-terceros-vista.php
@@ -1,6 +1,50 @@
-<section class="terceros-vista">
-<div class="enlaces-home">
-    <a href="http://localhost/TERCEROS/home"><img href="<?php echo SERVERURL;?>home">VOLVER</a>
+<section class="lista-terceros-vista">
+<!-- <div class="enlaces-home"> -->
+    <!-- <a href="http://localhost/TERCEROS/home"><img href="<?php echo SERVERURL;?>home">VOLVER</a>
         
-    </div>
+    </div> -->
+    <table class="tabla" >
+        <thead class="tabla-header">
+            <tr>
+                <th>IDENTIFICACIÓN</th>
+                <th>TIPO</th>
+                <th>NOMBRE COMPLETO</th>
+                <th>TELÉFONO</th>
+                <th>CORREO</th>
+                <th>DIRECCION</th>
+                <th>ESTADO</th>
+                <TH>ACCIONES</TH>
+            </tr>
+        </thead>
+        <tbody class="tabla-body">
+            <tr>
+                <td>1005912740</td>
+                <td>CC</td>
+                <td>Daniel Ricardo Diaz OValle</td>
+                <td>3112002176</td>
+                <td>danieldiazovalle.11@gmail.com</td>
+                <td>calle 183b #8-37</td>
+                <td>Activo</td>
+                <td>
+                    <button type="submit">editar</button>
+                    <button type="submit">eliminar</button>
+                </td>
+            </tr>
+        </tbody>
+        <tfoot class="tabla-footer">
+            <tr>
+                <td>
+                    <ul>
+                        <li> << </li>
+                        <li> <  </li>
+                        <li> 1  </li>
+                        <li> 2  </li>
+                        <li> >  </li>
+                        <li> >> </li>
+                    </ul>
+                </td>
+            </tr>
+        </tfoot>
+    </table>
+
 </section>
